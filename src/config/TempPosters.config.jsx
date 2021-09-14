91b3627bb591fc059dcbe3fe1b7e1bfd55c69c53
Anusree18 <anusreeh11@gmail.com>
2021-09-14 22:58:23 +0530

    adding more contents

--- src/config/TempPosters.config.jsx
+++ src/config/TempPosters.config.jsx
@@ -1,14 +1,6 @@
-import React from "react";
-import Slider from "react-slick";
+     import React from "react";
 
-//components
-import Poster from "../Poster/poster.component";
-
-//configs
-import PosterCarousalSettings from "../../config/PosterCarousel.config";
-
-export const Premier = () => {      
-      const PremierImages = [
+     const PremierImages = [
         {       
             src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",       
             alt: "zack",       
@@ -58,20 +50,5 @@ export const Premier = () => {
             subtitle: "english",    
         },
       ];
-    return(
-    <>
-    <div className="flex flex-col items-start">
-    <h3 className="text-white text-xl font-bold">Premires</h3>
-    <p className="text-white text-sm">Band new release every friday</p>
-    </div>
 
-     <Slider {...PosterCarousalSettings}>
-      {PremierImages.map((image) => (
-        <Poster {...image} isDark />)
-    )};
-     </Slider>
-    </>
-);
-};
-
-export default Premier;
+    export default PremierImages;
